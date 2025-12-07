0558d0e249e520d3659318357952270ad52c8e89
iamcelestino <trcelestino488@gmail.com>
2025-12-07 06:12:49 -0600

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return src('sass/**/*.scss')
     .pipe(sass())
-    .pipe(dest('css'))
+    .pipe(dest('styles'))
 }
 
 function watchTask() {
